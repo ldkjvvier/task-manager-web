6b4c18106b9f22273cb18c316a6154a71a6a7343
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2025-02-24 17:47:07 -0300

    replace mock task data with API call to fetch tasks in TaskProvider

--- src/provider/TaskProvider.tsx
+++ src/provider/TaskProvider.tsx
@@ -3,33 +3,11 @@ import { TaskContext } from '../context/TaskContext'
 import { Task } from '../models/task'
 
 const fetchTasksFromAPI = async (): Promise<Task[]> => {
-	const TASKMOCK: Task[] = [
-		{
-			id: '1',
-			title: 'Tarea 1',
-			description: 'Descripcion de la tarea 1',
-			dueDate: new Date(),
-			status: 'pending',
-			priority: 'high',
-		},
-		{
-			id: '2',
-			title: 'Tarea 2',
-			description: 'Descripcion de la tarea 2',
-			dueDate: new Date(),
-			status: 'pending',
-			priority: 'medium',
-		},
-		{
-			id: '3',
-			title: 'Tarea 3',
-			description: 'Descripcion de la tarea 3',
-			dueDate: new Date(),
-			status: 'completed',
-			priority: 'low',
-		},
-	]
-	return TASKMOCK
+	const res = await fetch('http://localhost:5000/api/task')
+	if (res.status !== 200) {
+		throw new Error('Error al cargar las tareas')
+	}
+	return res.json()
 }
 
 const createTaskInAPI = async (task: Task): Promise<Task> => {
@@ -73,6 +51,7 @@ const TaskProvider = ({ children }: { children: ReactNode }) => {
 		const loadTasks = async () => {
 			try {
 				const tasks = await fetchTasksFromAPI()
+				console.log(tasks)
 				setTasks(tasks)
 			} catch (err) {
 				console.error('Error al cargar las tareas', err)
