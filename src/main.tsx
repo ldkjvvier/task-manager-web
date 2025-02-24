ba0d9e1c4cb4816ce4e3308c599e27d956d779bb
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2025-02-24 14:08:37 -0300

    add CssBaseline component for consistent styling across the application

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import './index.css'
 import { CustomTheme } from './theme/theme.tsx'
 import { AppRoutes } from './config/routes.tsx'
 import AuthProvider from './provider/AuthProvider.tsx'
+import { CssBaseline } from '@mui/material'
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<AuthProvider>
 			<CustomTheme>
+				<CssBaseline />
 				<AppRoutes />
 			</CustomTheme>
 		</AuthProvider>
