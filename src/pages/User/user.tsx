1d185f1a5b91952027fea368ea59a925a21495ce
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2025-02-24 21:53:56 -0300

    feat: add category tabs and functionality for task filtering in User component

--- src/pages/User/user.tsx
+++ src/pages/User/user.tsx
@@ -5,6 +5,9 @@ import {
 	Typography,
 	Container,
 	Box,
+	Tabs,
+	Tab,
+	IconButton,
 } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { TaskForm } from './components/taskForm'
@@ -12,11 +15,20 @@ import TaskList from './components/taskList'
 import { CustomSnackbar } from './components/SnackBar'
 import { isTaskNearDueDate } from '../../helper/TaskNearDueDate'
 import { useTask } from '../../hooks/useTask'
-
+import { Add } from '@mui/icons-material'
 export const User = () => {
 	const { tasks, addTask, removeTask, updateTask } = useTask()
 	const [open, setOpen] = useState(false)
 	const [message, setMessage] = useState('')
+
+	const [currentCategory, setCurrentCategory] = useState('todas')
+	const [categories, setCategories] = useState([
+		'trabajo',
+		'personal',
+		'compras',
+	])
+	const [isAddingCategory, setIsAddingCategory] = useState(false)
+	const [newCategory, setNewCategory] = useState('')
 
 	useEffect(() => {
 		const nearDueTasks = tasks.filter((task) =>
@@ -76,9 +88,47 @@ export const User = () => {
 					<Typography variant="h6">Gestor de Tareas</Typography>
 				</Toolbar>
 			</AppBar>
+
 			<Container maxWidth="md">
 				<Box my={4} textAlign={'start'}>
 					<TaskForm onAddTask={handleAddTask} />
+
+					<Box
+						sx={{
+							borderBottom: 1,
+							borderColor: 'divider',
+							mt: 4,
+							display: 'flex',
+							alignItems: 'center',
+						}}
+					>
+						<Tabs
+							value={currentCategory}
+							onChange={(_, newValue) => setCurrentCategory(newValue)}
+							variant="scrollable"
+							scrollButtons="auto"
+						>
+							<Tab label="Todas" value="todas" />
+							{categories.map((category) => (
+								<Tab
+									key={category}
+									label={
+										category.charAt(0).toUpperCase() +
+										category.slice(1)
+									}
+									value={category}
+								/>
+							))}
+						</Tabs>
+						<IconButton
+							size="small"
+							onClick={() => setIsAddingCategory(true)}
+							sx={{ ml: 1 }}
+						>
+							<Add />
+						</IconButton>
+					</Box>
+
 					<TaskList
 						tasks={tasks}
 						onToggleCompletion={handleToggleTaskCompletion}
